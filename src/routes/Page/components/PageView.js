460d3ac1b1ed27e3b2a9be8840d5c16f6dbc6c63
huangjunjie <946008612@qq.com>
2018-02-26 11:30:02 +0800

    page center

--- src/routes/Page/components/PageView.js
+++ src/routes/Page/components/PageView.js
@@ -5,7 +5,27 @@ import './PageView.scss'
 export const PageView = () => (
   <div>
     <Helmet title='Page' />
-    <h4>Page</h4>
+    <div className='page-wrapper'>
+      <div className='page-list'>list</div>
+      <div className='page-nav'>
+        nav
+      </div>
+      <div className='page-center'>
+        <div className='page-title'>标题标题</div>
+        <div className='page-tags'>
+          <ul>
+            <li className='tag-item'>tag</li>
+            <li className='tag-item'>tag</li>
+            <li className='tag-item'>tag</li>
+          </ul>
+        </div>
+        <div className='page-detail'>
+          <span>游者J</span>
+          <span>2018-02-26 11:22</span>
+        </div>
+        <div className='page-content'>content</div>
+      </div>
+    </div>
   </div>
 )
 
