307d65ea310203c28ad3f5e51be46376e819236b
huangjunjie <946008612@qq.com>
2018-02-26 09:34:14 +0800

    header adjust

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,21 +4,23 @@ import './Header.scss'
 
 export const Header = () => (
   <div className='header clearfix'>
-    <div className='blog-icon'>
-      <IndexLink to='/' activeClassName='activeRoute'>YOUZHEJ</IndexLink>
+    <div className='header-wrapper'>
+      <div className='blog-icon'>
+        <IndexLink to='/' activeClassName='activeRoute'>YOUZHEJ</IndexLink>
+      </div>
+      <ul className='header-list clearfix'>
+        <li>
+          <Link to='/list' activeClassName='activeRoute'>
+            <i className='iconfont icon-navlist' />
+          </Link>
+        </li>
+        <li>
+          <Link to='/edit' activeClassName='activeRoute'>
+            <i className='iconfont icon-survey' />
+          </Link>
+        </li>
+      </ul>
     </div>
-    <ul className='header-list clearfix'>
-      <li>
-        <Link to='/list' activeClassName='activeRoute'>
-        <i className="iconfont icon-navlist"></i>
-        </Link>
-      </li>
-      <li>
-        <Link to='/edit' activeClassName='activeRoute'>
-          <i className="iconfont icon-survey"></i>
-        </Link>
-      </li>
-    </ul>
   </div>
 )
 
