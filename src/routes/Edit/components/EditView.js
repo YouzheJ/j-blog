11e259711c67622c6aeb45cbda5a27cd4449c3da
huangjunjie <946008612@qq.com>
2018-02-26 16:28:53 +0800

    fix bug: textarea tab

--- src/routes/Edit/components/EditView.js
+++ src/routes/Edit/components/EditView.js
@@ -8,7 +8,13 @@ class EditView extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      viewContent: `<div class="md-base" data-reactid="24"><h1 id="标题">标题</h1>
+      viewContent: `<div class="md-base" data-reactid="24">
+      <h1 id="标题">标题</h1>
+      <h2 id="标题2">标题2</h2>
+      <h3 id="标题3">标题3</h3>
+      <h4 id="标题4">标题4</h4>
+      <h5 id="标题5">标题5</h5>
+      <h6 id="标题6">标题6</h6>
       <ul>
       <li>aa</li>
       <li>bb</li>
@@ -16,20 +22,29 @@ class EditView extends React.Component {
       <hr>
       <pre><code>function test () {
        // todo
-      }</code></pre></div>`
+      }</code></pre></div>`,
+      mdText: ''
     }
   }
   onChange = (e) => {
     let value = e.target.value
-    console.log(value)
-    this.setState({viewContent: this.converter.makeHtml(value)})
+    this.setState({viewContent: this.converter.makeHtml(value), mdText: value})
+  }
+  onKeyDown = (e) => {
+    let { value, selectionStart, selectionEnd } = e.target
+    if (e.keyCode === 9 || e.which === 9) {
+      e.preventDefault()
+      value = value.substring(0, selectionStart) + '\t' + value.substring(selectionEnd)
+      e.target.selectionEnd = selectionStart + 1
+      this.setState({mdText: value})
+    }
   }
   componentWillMount () {
     this.converter = new showdown.Converter()
     this.converter.setFlavor('github')
   }
   render () {
-    const { viewContent } = this.state
+    const { mdText, viewContent } = this.state
     return (
       <div className='edit-page clearfix'>
         <Helmet title='Edit' />
@@ -38,7 +53,9 @@ class EditView extends React.Component {
             <textarea
               className='edit-area'
               placeholder='markdown editor'
+              value={mdText}
               onChange={this.onChange}
+              onKeyDown={this.onKeyDown}
             />
           </div>
         </div>
