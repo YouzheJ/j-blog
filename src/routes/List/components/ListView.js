aae1ac582ae81e8435862eaa549d1dec4935cd2a
youzhej <youzhej@youzhejdeMacBook-Pro.local>
2018-05-03 00:13:41 +0800

    list

--- src/routes/List/components/ListView.js
+++ src/routes/List/components/ListView.js
@@ -2,28 +2,63 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import { Link } from 'react-router'
 import './ListView.scss'
+import constant from '../../../constant'
+import axios from 'axios'
 
-export const ListView = (props) => {
-  const { total, list } = props.list;
-  return <div>
-    <Helmet title='List' />
-    <div className='list-wrapper'>
-      <div className='list-header'>
-        <div className='title'>youzheJ-blog</div>
-        <p className='slogan'>什么都想学学，什么都要写写。</p>
-        <div className='logo'></div>
-      </div>
-      <div className='list-content'>
-        <div className='page-list-wrapper'>
-          <ul className='page-list'>
-          {
-            list && list.length && list.map(createList)
+const { LIST_GET } = constant.api;
+
+class ListView extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  getListData = (index) => {
+    return new Promise((resolve, reject) => {
+      axios.get(`${LIST_GET}?index=${index}`)
+        .then(({data}) => {
+          if (data && data.success && data.data) {
+            resolve(data.data)
+          } else {
+            resolve({});
           }
-          </ul>
+        })
+        .catch((err) => {
+          // 重定向到500页
+          resolve({});
+        });
+    });
+  }
+  componentDidMount () {
+    const { updateList, location } = this.props;
+    let index = Number(location.query.index);
+    isNaN(index) && (index = 0);
+    this.getListData(index).then((res) => updateList(res));
+  }
+  render () {
+    const { total, list } = this.props.list;
+    return (<div>
+      <Helmet title='List' />
+      <div className='list-wrapper'>
+        <div className='list-header'>
+          <div className='title'>youzheJ-blog</div>
+          <p className='slogan'>什么都想学学，什么都要写写。</p>
+          <div className='logo'></div>
+        </div>
+        <div className='list-content'>
+          <div className='page-list-wrapper'>
+            <ul className='page-list'>
+            {
+              list && list.length && list.map(createList)
+            }
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+    )
+  }
 }
 
 const createList = (item) => (
--- src/routes/List/modules/list.js
+++ src/routes/List/modules/list.js
@@ -1,46 +1,23 @@
 // ------------------------------------
 // Constants
 // ------------------------------------
-export const COUNTER_INCREMENT = 'COUNTER_INCREMENT'
+export const UPDATE_LIST = 'UPDATE_LIST'
 
 // ------------------------------------
 // Actions
 // ------------------------------------
-export function increment (value = 1) {
+export function updateList (value) {
   return {
-    type: COUNTER_INCREMENT,
+    type: UPDATE_LIST,
     payload: value
   }
-}
-
-/*  This is a thunk, meaning it is a function that immediately
-    returns a function for lazy evaluation. It is incredibly useful for
-    creating async actions, especially when combined with redux-thunk!
-
-    NOTE: This is solely for demonstration purposes. In a real application,
-    you'd probably want to dispatch an action of COUNTER_DOUBLE and let the
-    reducer take care of this logic.  */
-
-export const doubleAsync = () => {
-  return (dispatch, getState) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        dispatch(increment(getState().counter))
-        resolve()
-      }, 200)
-    })
-  }
-}
-
-export const actions = {
-  //
 }
 
 // ------------------------------------
 // Action Handlers
 // ------------------------------------
 const ACTION_HANDLERS = {
-  // [COUNTER_INCREMENT]: (state, action) => state + action.payload
+  [UPDATE_LIST]: (state, action) => ({...state, ...action.payload}),
 }
 
 // ------------------------------------
@@ -49,6 +26,5 @@ const ACTION_HANDLERS = {
 export const initialState = {}
 export default function (state = initialState, action) {
   const handler = ACTION_HANDLERS[action.type]
-
   return handler ? handler(state, action) : state
 }
