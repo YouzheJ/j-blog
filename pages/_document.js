b5989ae95b48a60bedda21a5a99f37dd68a6206b
youzhej <youzhej@youzhejdeMacBook-Pro.local>
2018-06-06 00:32:09 +0800

    百度统计脚本

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,15 @@ export default class MyDocument extends Document {
           <Header />
           <Main />
           <NextScript />
+          <script>
+            var _hmt = _hmt || [];
+            (function() {
+              var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?1d7a474f78f03e11dca9a834abef384e";
+              var s = document.getElementsByTagName("script")[0]; 
+              s.parentNode.insertBefore(hm, s);
+            })();
+          </script>
         </body>
       </html>
     )
