e20273bbc64bf6c29cb8d0153e13ab678eec8f32
youzhej <youzhej@youzhejdeMacBook-Pro.local>
2018-06-06 21:30:28 +0800

    去掉debug

--- static/baidu.js
+++ static/baidu.js
@@ -1,5 +1,4 @@
 (function() {
-  debugger
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?1d7a474f78f03e11dca9a834abef384e";
   var s = document.getElementsByTagName("script")[0]; 
